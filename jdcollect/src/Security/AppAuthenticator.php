4b2371c2106163e013dc73e3f223b057fa90b026
Lucas Fauchart <lucasfauchart75@gmail.com>
2024-12-04 12:05:51 +0100

    Changement

--- jdcollect/src/Security/AppAuthenticator.php
+++ jdcollect/src/Security/AppAuthenticator.php
@@ -49,7 +49,7 @@ class AppAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        return new RedirectResponse($this->urlGenerator->generate('app_login'));
+        return new RedirectResponse($this->urlGenerator->generate('app_acceuil'));
         throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
